dd60b2b330ded97bf655cc4900584661d51da104
delta-12 <btesq235@gmail.com>
2024-06-15 18:41:07 -0400

    IoT-Bit-V6.0 MessageLayer

---
+++ Source-Code/MessageLayer/generate.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+OUT_DIR=./out
+if [ ! -d "$OUT_DIR" ]; then
+    echo "Creating output directory"
+    mkdir -p $OUT_DIR
+fi
+
+echo "Generating message layer protobufs for python..."
+PYTHON_OUT_DIR=$OUT_DIR/python
+if [ ! -d "$PYTHON_OUT_DIR" ]; then
+    echo "Creating Python output directory"
+    mkdir -p $PYTHON_OUT_DIR
+fi
+protoc -I $PWD --python_out=$PYTHON_OUT_DIR $PWD/*.proto
+
+echo "Generating message layer protobufs for C..."
+C_OUT_DIR=$OUT_DIR/c
+if [ ! -d "$C_OUT_DIR" ]; then
+    echo "Creating C output directory"
+    mkdir -p $C_OUT_DIR
+fi
+python3 nanopb/generator/nanopb_generator.py -I $PWD $PWD/*.proto -D $C_OUT_DIR
